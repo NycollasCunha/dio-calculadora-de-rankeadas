f6bf7895289c8716f356b69f762b7635d6283b5f
NycollasCunha <nyteduardo@gmail.com>
2026-01-20 23:04:08 -0300

    Calculadora de saldo e calculadora de nível concluídos. Projeto finalizado

--- index.js
+++ index.js
@@ -1,9 +1,47 @@
-let level = "Ferro"
-let winRatio = CalculateWinRatio(25, 20)
+let winRatio = CalculateWinRatio(1001,100)
+let playerLevel = CalculateLevel(winRatio)
+
+console.log("O player tem um saldo de: " + winRatio + " e está no nível: " + playerLevel + "!")
 
 
 function CalculateWinRatio(wins, losses)
 {
   let ratio = wins - losses
-  return console.log(ratio)
+  return ratio
+}
+
+function CalculateLevel(ratio)
+{
+  let level = ""
+
+  if (ratio <= 10)
+  {
+    level = "Ferro"
+  }
+  else if (ratio <= 20)
+  {
+    level = "Bronze"
+  }
+  else if (ratio <= 50)
+  {
+    level = "Prata"
+  }
+  else if (ratio <= 80)
+  {
+    level = "Ouro"
+  }
+  else if (ratio <= 90)
+  {
+    level = "Diamante"
+  }
+  else if (ratio <= 100)
+  {
+    level = "Lendário"
+  }
+  else
+  {
+    level = "Imortal"
+  }
+
+  return level
 }
